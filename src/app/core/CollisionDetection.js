e2c4a954ef1075d169bb61f210a38b09198a266b
Marina Diubailo <marinadiubailo@gmail.com>
2024-12-20 18:29:55 +0300

    refactor: correct collision logic

--- src/app/core/CollisionDetection.js
+++ src/app/core/CollisionDetection.js
@@ -13,18 +13,17 @@ export class CollisionDetection {
     return false;
   }
 
-  // Check ball collision with bricks
   isCollidingBricks(ball, bricks) {
     let colliding = false;
 
     bricks.forEach((brick, i) => {
       if (this.isCollidingBrick(ball, brick)) {
         ball.changeYDirection();
-
         if (brick.energy === 1) {
           bricks.splice(i, 1);
         } else {
           brick.energy -= 1;
+          brick.opacity /= 2;
         }
         colliding = true;
       }
@@ -33,30 +32,28 @@ export class CollisionDetection {
   }
 
   checkBallCollision(ball, paddle) {
-    // 1. Check ball collision with paddle
+    /* Мяч сталкивается с платформой */
     if (
-      (ball.pos.x + ball.width > paddle.pos.x &&
-        ball.pos.x < paddle.pos.x + paddle.width &&
-        ball.pos.y + ball.height === paddle.pos.y) ||
-      ball.pos.y < 0
+      ball.pos.x + ball.width >= paddle.pos.x &&
+      ball.pos.x <= paddle.pos.x + paddle.width &&
+      ball.pos.y + ball.height === paddle.pos.y
     ) {
       ball.changeYDirection();
+      console.log('paddle hit');
     }
-    // 2. Check ball collision with walls
-    // Ball movement X constraints
+
+    /* Мяч сталкивается с боковыми краями поля */
     if (ball.pos.x > canvasSize.width - ball.width || ball.pos.x < 0) {
       ball.changeXDirection();
     }
-    // Ball movement Y constraints
-    // if (ball.pos.y < 0) {
-    //   ball.changeYDirection();
-    // }
+
+    /* Мяч сталкивается с верхним краем поля */
+    if (ball.pos.y < 0) {
+      ball.changeYDirection();
+    }
   }
 
   isBottomCollision(ball) {
-    if (ball.pos.y > canvasSize.height) {
-      return true;
-    }
-    return false;
+    return ball.pos.y > canvasSize.height;
   }
 }
