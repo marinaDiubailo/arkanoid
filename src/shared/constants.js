76fa808c1665c999d2fe6f31050085f601c52a47
Marina Diubailo <marinadiubailo@gmail.com>
2024-12-20 18:20:14 +0300

    chore: adapt consts for small screens

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,28 +1,31 @@
-const canvas = document.getElementById('canvas');
 const wrapper = document.getElementById('wrapper');
 
 const CANVAS_WIDTH = 1000;
 const OFFSET = 10;
 const ASPECT_RATIO = 3 / 5;
 const CANVAS_HEIGTH = CANVAS_WIDTH * ASPECT_RATIO;
+const INITIAL_BALL_OFFSET = 5;
+const INITIAL_BALL_SPEED = 5;
 
 export const STAGE_PADDING = 10;
-export const STAGE_ROWS = 20;
+export const STAGE_ROWS = 16;
 export const STAGE_COLS = 10;
-export const BRICK_PADDING = 5;
-export const PADDLE_SPEED = 10;
-export const BALL_SPEED = 5;
+export const BRICK_PADDING = 2;
+
+export const PADDLE_SPEED = 5;
 
 const paddleDimensions = {
   WIDTH: null,
   HEIGHT: null,
   START_X: null,
+  START_Y: null,
 };
 
 const ballDimensions = {
   SIZE: null,
   START_X: null,
   START_Y: null,
+  SPEED: INITIAL_BALL_SPEED,
 };
 
 const brickDimensions = {
@@ -33,9 +36,12 @@ const brickDimensions = {
 const canvasSize = {
   width: CANVAS_WIDTH,
   height: CANVAS_HEIGTH,
+  OFFSET_LEFT: null,
 };
 
 function adjustDimensions() {
+  const canvas = document.getElementById('canvas');
+
   const windowWidth = window.innerWidth;
   const windowHeight = window.innerHeight;
 
@@ -50,6 +56,9 @@ function adjustDimensions() {
 
   canvas.width = canvasSize.width;
   canvas.height = canvasSize.height;
+
+  const rect = canvas.getBoundingClientRect();
+  canvasSize.OFFSET_LEFT = rect.left;
 
   wrapper.style.width = canvasSize.width + 'px';
   wrapper.style.height = canvasSize.height + 'px';
@@ -66,11 +75,16 @@ function adjustDimensions() {
   paddleDimensions.START_X = Math.floor(
     canvasSize.width / 2 - paddleDimensions.WIDTH / 2,
   );
+  paddleDimensions.START_Y = canvasSize.height - paddleDimensions.HEIGHT;
 
   ballDimensions.SIZE = Math.floor(canvasSize.height / 20);
   ballDimensions.START_X = Math.floor(canvasSize.width / 2);
   ballDimensions.START_Y =
-    canvasSize.height - paddleDimensions.HEIGHT - ballDimensions.SIZE - 10;
+    canvasSize.height -
+    paddleDimensions.HEIGHT -
+    ballDimensions.SIZE -
+    (canvasSize.width < 720 ? 3 : INITIAL_BALL_OFFSET);
+  ballDimensions.SPEED = canvasSize.width < 720 ? 3 : INITIAL_BALL_SPEED;
 }
 
 window.onload = adjustDimensions;
