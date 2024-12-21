1567736fe57d8d769dbc325d3e5872e8e63d024d
Marina Diubailo <marinadiubailo@gmail.com>
2024-12-21 17:37:29 +0300

    feat: initiate App singleton

--- src/app/App.js
+++ src/app/App.js
@@ -2,153 +2,69 @@ import './styles/index.scss';
 
 import { Game, GAME_WON } from './core/Game.js';
 
-const totalCountPannel = document.getElementById('total');
-const statsPannel = document.getElementById('stats');
-const canvasWrapper = document.getElementById('wrapper');
-const levelButtons = document.querySelectorAll('.levels button');
-
-let level = 1;
-let totalScore = 0;
-
-function unlockNextLevel() {
-  if (level <= levelButtons.length) {
-    levelButtons[level - 1].disabled = false;
-  }
-}
-
-function onGameFinished(result, score) {
-  if (result === GAME_WON) {
-    totalScore += score;
-    totalCountPannel.innerText = totalScore;
-    level++;
-    unlockNextLevel();
-  }
-
-  setTimeout(() => {
-    statsPannel.style.display = 'flex';
-    canvasWrapper.style.visibility = 'hidden';
-  }, 2000);
-}
-
-function startNewGame(level) {
-  const game = new Game(level, onGameFinished);
-  game.start();
-}
-
-levelButtons.forEach((button, index) => {
-  button.addEventListener('click', () => {
-    if (!button.disabled) {
-      level = index + 1;
-      statsPannel.style.display = 'none';
-      canvasWrapper.style.visibility = 'visible';
-      startNewGame(level);
+class App {
+  constructor() {
+    if (App.instance) {
+      return App.instance;
     }
-  });
-});
 
-// levelButtons.forEach((button, index) => {
-//   button.addEventListener('click', () => {
-//     if (!button.disabled) {
-//       level = index + 1;
-//       statsPannel.style.display = 'none';
-//       canvasWrapper.style.visibility = 'visible';
-//       startGame(playGround, level);
-//     }
-//   });
-// });
+    this.totalCountPannel = document.getElementById('total');
+    this.startPannel = document.getElementById('start');
+    this.canvasWrapper = document.getElementById('wrapper');
+    this.levelButtons = document.querySelectorAll('#levels button');
 
-// canvas.addEventListener('click', () => {
-//   isGameStarted = true;
-// });
+    this.level = 1;
+    this.totalScore = 0;
 
-// document.addEventListener('keydown', (e) => {
-//   if (e.code === 'Space' || e.key === 'Space') {
-//     e.preventDefault();
-//     if (!isGameStarted) {
-//       isGameStarted = true;
-//     }
-//   }
-// });
+    this.#init();
 
-// function setGameFinished(result) {
-//   const delay = 2000;
+    App.instance = this;
+  }
 
-//   setTimeout(() => {
-//     if (result === GAME_WON) {
-//       totalScore += score;
-//       totalCountPannel.innerText = totalScore;
-//       level++;
-//       unlockNextLevel();
-//     }
-//     statsPannel.style.display = 'flex';
-//     canvasWrapper.style.visibility = 'hidden';
+  #init() {
+    this.levelButtons.forEach((button, index) => {
+      button.addEventListener('click', () => {
+        if (!button.disabled) {
+          this.level = index + 1;
+          this.startPannel.style.display = 'none';
+          this.canvasWrapper.style.visibility = 'visible';
+          this.#startNewGame(this.level);
+        }
+      });
+    });
+  }
 
-//     isGameStarted = false;
-//     score = 0;
-//     lives = 3;
-//   }, delay);
-// }
+  #unlockNextLevel() {
+    if (this.level <= this.levelButtons.length) {
+      this.levelButtons[this.level - 1].disabled = false;
+    }
+  }
 
-// function gameLoop(playGround, bricks, paddle, ball, collision, level) {
-//   playGround.clear();
-//   playGround.drawBricks(bricks);
-//   playGround.drawItem(paddle);
-//   playGround.drawItem(ball);
+  #onGameFinished(result, score) {
+    if (result === GAME_WON) {
+      this.totalScore += score;
+      this.totalCountPannel.innerText = this.totalScore;
+      this.level++;
+      this.#unlockNextLevel();
+    }
 
-//   if (
-//     (paddle.isMovingLeft && paddle.pos.x > 0) ||
-//     (paddle.isMovingRight && paddle.pos.x < canvasSize.width - paddle.width)
-//   ) {
-//     paddle.move();
-//   }
+    setTimeout(() => {
+      this.startPannel.style.display = 'flex';
+      this.canvasWrapper.style.visibility = 'hidden';
+    }, 2000);
+  }
 
-//   if (isGameStarted) {
-//     ball.move();
-//   } else {
-//     ball.updateXPosition(paddle.pos.x + paddle.width / 2 - ball.width / 2);
-//   }
+  #startNewGame(level) {
+    const game = new Game(level, this.#onGameFinished.bind(this));
+    game.start();
+  }
 
-//   collision.checkBallCollision(ball, paddle);
-//   const collidingBrick = collision.isCollidingBricks(ball, bricks);
-//   const bottomCollision = collision.isBottomCollision(ball);
+  static getInstance() {
+    if (!App.instance) {
+      App.instance = new App();
+    }
+    return App.instance;
+  }
+}
 
-//   if (collidingBrick) {
-//     score += 1;
-//     playGround.drawScore(score);
-//   }
-
-//   if (bottomCollision) {
-//     lives--;
-//     isGameStarted = false;
-//     paddle.resetPosition();
-//     ball.resetPosition();
-//     playGround.drawLives(lives);
-//   }
-
-//   if (lives < 0) {
-//     playGround.drawGameResult(GAME_OVER);
-//     playGround.clearScreen();
-//     return setGameFinished(GAME_OVER);
-//   }
-
-//   if (bricks.length === 0) {
-//     playGround.drawGameResult(GAME_WON);
-//     playGround.clearScreen();
-//     return setGameFinished(GAME_WON);
-//   }
-
-//   requestAnimationFrame(() =>
-//     gameLoop(playGround, bricks, paddle, ball, collision, level),
-//   );
-// }
-
-// function startGame(playGround, level) {
-//   playGround.drawScore(score);
-//   playGround.drawLives(lives);
-
-//   const bricks = createBricks(getLevelMap(level));
-//   const paddle = new Paddle();
-//   const ball = new Ball();
-
-//   gameLoop(playGround, bricks, paddle, ball, new CollisionDetection(), level);
-// }
+App.getInstance();
